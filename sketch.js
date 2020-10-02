9b7be041c9d1d19e3f6bf023b372f539c2a22a1f
ArnavBhattacharya <70324531+ArnavBhattacharya@users.noreply.github.com>
2020-10-02 17:22:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var ground
+
+function setup() {
+   var canvas = createCanvas(800,400);
+   
+   ground = new Ground(600,height,1200,20)
+}
+
+function draw() {
+  background(255,255,255);  
+  ground.display();
+  drawSprites();
+}
